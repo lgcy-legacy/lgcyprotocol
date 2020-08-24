197cdb8ad3145d28c4adc0555e1541da1311a135
MeetSupernova <lgcy@tronsecure.io>
2020-08-25 01:31:39 +0530

    First commit

---
+++ framework/src/main/java/org/tron/program/Version.java
@@ -0,0 +1,13 @@
+package org.tron.program;
+
+public class Version {
+
+  public static final String versionName = "GreatVoyage-v4.0.0-8-g67170b635";
+  public static final String versionCode = "13210";
+  private static final String version = "4.0.1";
+
+  public static String getVersion() {
+    return version;
+  }
+}
+
